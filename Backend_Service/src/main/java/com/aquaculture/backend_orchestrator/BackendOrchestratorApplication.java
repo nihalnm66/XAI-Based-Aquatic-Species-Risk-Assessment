88a34b0b7bbab61813d8b33210c00a360ca06077
Nihal N M <nihalnm072005@gmail.com>
2026-08-14 12:28:27 +0530

    Organize Spring Boot files correctly inside Backend_Service

---
+++ Backend_Service/src/main/java/com/aquaculture/backend_orchestrator/BackendOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.aquaculture.backend_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOrchestratorApplication.class, args);
+	}
+
+}
---
+++ Backend_Service/src/test/java/com/aquaculture/backend_orchestrator/BackendOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquaculture.backend_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
